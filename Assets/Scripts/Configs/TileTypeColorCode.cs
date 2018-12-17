3df1a0ea9228b69b004d3eb47f77c080e7306a43
AVAVT <itsavavt@gmail.com>
2018-12-17 17:02:16 +0700

    Odin Inspector

--- Assets/Scripts/Configs/TileTypeColorCode.cs
+++ Assets/Scripts/Configs/TileTypeColorCode.cs
@@ -1,8 +1,9 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 [CreateAssetMenu(fileName = "TileTypeColorCode", menuName = "C4T-Hackathon/TileTypeColorCode", order = 0)]
-public class TileTypeColorCode : ScriptableObject
+public class TileTypeColorCode : SerializedScriptableObject
 {
   public Dictionary<Color, TileType> values;
 }
